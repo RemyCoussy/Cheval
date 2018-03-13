0078f62d530bab899ee748ce43dddff019284d33
Remy <r.coussy@agence42.fr>
2018-03-13 10:01:58 +0100

    debug

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 	$name=$_POST['user'];
 	$password=$_POST['pass'];
 	try{
-		$pdo = new PDO('mysql:host=localhost;dbname=cheval', $name, $password);
+		$pdo = new PDO('mysql:host=localhost;dbname=chevaux', $name, $password);
 	$sql="SHOW TABLES";
 	$statement = $pdo->prepare($sql);
 	$statement->execute();
--- table.php
+++ table.php
@@ -1,46 +1,9 @@
 <?php
-require 'header.php';
-
-$id = $_GET[id];
-
 session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=cheval', $_SESSION['user'], $_SESSION['pass']);
 
-/* Requête nom des colonnes */
-$statement = $pdo->query('DESCRIBE ' . $id);
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-/* Requête données */
-$sql = "SELECT * FROM $id";
-$statement = $pdo->prepare($sql);
-$statement->execute();
-$tables = $statement->fetchAll(PDO::FETCH_NUM);
-$num = $statement->columnCount();
-
-
-/* Affichage nom des colonnes */
-echo '<table class="table table-hover table-striped table-bordered"><thead><tr>';
-
-foreach($result as $column){
-	echo '<th>'.$column['Field'].'</th>';
-}
-
-echo "</tr></thead>";
-
-/* Affichage données */
-foreach($tables as $table){
-	echo '<tr>';
-	for($i=0;$i<$num;$i++){
-		if($table[$i] === NULL){
-			echo '<td>NULL</td>';
-		}else{
-			echo '<td>'.$table[$i].'</td>';
-		}
-    }
-    echo '</tr>';
-}
-
-echo "</table>";
-
-require 'footer.php';
+require 'util.php';
+require 'model/table.php';
+$data['columns'] = getColumns();
+$data['data'] = getData();
+getBlock('view/table.php', $data);
 ?>
