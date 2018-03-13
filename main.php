412cbea7873eb99aac72f564d51c4eff1b3c9279
Remy <r.coussy@agence42.fr>
2018-03-13 08:57:47 +0100

    lien vers table

--- main.php
+++ main.php
@@ -28,7 +28,7 @@ echo '<table class="table">';
 //Loop through our table names.
 foreach($tables as $table){
     //Print the table name out onto the page.
-    echo '<tr><td><a href="/Cheval/table.php?id='.$table[0].'">'.$table[0].'</a></td></tr>';
+    echo '<tr><td><a href="/table.php?id='.$table[0].'">'.$table[0].'</a></td></tr>';
 }
 
 echo "</table>";
