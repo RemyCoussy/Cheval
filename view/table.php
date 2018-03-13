41f69601bb22243a9b2573d73f03e84400897d76
Remy <r.coussy@agence42.fr>
2018-03-13 11:29:36 +0100

    boutons d'action

--- view/table.php
+++ view/table.php
@@ -1,26 +1,28 @@
 <?php
 require 'header.php';
 
-/* Affichage nom des colonnes */
-echo '<table class="table table-hover table-striped table-bordered"><thead><tr>';
+echo '<button>Ajouter une ligne</button>';
+echo '<button>Supprimer la selection</button>';
 
+echo '<table class="table table-hover table-striped table-bordered"><thead><tr><th></th>';
+/* Affichage nom des colonnes */
 foreach($data['columns'] as $column){
-	echo '<th>'.$column['Field'].'</th>';
+	echo '<th>' . $column['Field'] . '</th>';
 }
 
-echo "</tr></thead>";
+echo "<th>Options</th></tr></thead>";
 
 /* Affichage données */
 foreach($data['data'] as $table){
-	echo '<tr>';
+	echo '<tr><td><input type="checkbox" name="delete" value=' . "$table[0]" . '></td>';
 	for($i=0;$i<$_SESSION['num'];$i++){
 		if($table[$i] === NULL){
 			echo '<td>NULL</td>';
 		}else{
-			echo '<td>'.$table[$i].'</td>';
+			echo '<td>' . $table[$i] . '</td>';
 		}
     }
-    echo '</tr>';
+    echo '<td><a href="/edit.php?id=' . "$table[0]" . '">Edit</a></td></tr>';
 }
 
 echo "</table>";
