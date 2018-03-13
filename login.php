0ea02910c4b1608c28e5e3eb01024b0ca6409ba3
bunnyhopping <bunnyhopping@live.com.mx>
2018-03-13 11:03:20 +0100

    login

--- login.php
+++ login.php
@@ -2,13 +2,14 @@
 	$name=$_POST['user'];
 	$password=$_POST['pass'];
 	try{
-		$pdo = new PDO('mysql:host=127.0.0.1;dbname=chevaux', $name, $password);
+		$pdo = new PDO('mysql:host=localhost;dbname=chevaux', $name, $password);
 	$sql="SHOW TABLES";
 	$statement = $pdo->prepare($sql);
 	$statement->execute();
 	session_start();
 	$_SESSION['user']=$name;
 	$_SESSION['pass']=$password;
+	echo "1";
 	}
 	catch(PDOException $e)
 	{
